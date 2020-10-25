2298327f26632d3933c000d61a483d9c96381b97
fivecoil <fivecoil@gmail.com>
2020-10-25 22:52:40 +0300

    Commit for transfer

--- Sources/NetworkService/NetworkProtocols/NetworkService.swift
+++ Sources/NetworkService/NetworkProtocols/NetworkService.swift
@@ -11,7 +11,7 @@ open class NetworkService<Input, Output> {
     
     var nextService: NetworkService = NetworkService()
     
-    func throwNext(_ data: NetworkResponse) -> OperationEntity<Input, Output> {
+    func throwNext(_ data: NetworkResponse) -> OperationEntity<Output> {
         OperationEntity()
     }
 
--- Sources/NetworkService/OperationEntity.swift
+++ Sources/NetworkService/OperationEntity.swift
@@ -7,4 +7,34 @@
 
 import Foundation
 
-open class OperationEntity<Input, Output> {}
+open class OperationEntity<Model> {
+    
+    // MARK: - Private properties
+    
+    private var completionHandler: ((Model) -> Void)?
+    private var errorHandler: ((Error) -> Void)?
+    private var writtenData: Model?
+    private var writtenError: Error?
+
+    
+    // MARK: - Public methods
+    
+    func onComplete(_ completionHandler: ((Model) -> Void)?) -> Self {
+        guard let dataInEntity = writtenData else {
+            return self
+        }
+        self.completionHandler = completionHandler
+        self.completionHandler?(dataInEntity)
+        return self
+    }
+    
+    func onError(_ errorHandler: ((Error) -> Void)?) -> Self {
+        guard let errorInEntity = writtenError else {
+            return self
+        }
+        self.errorHandler = errorHandler
+        self.errorHandler?(errorInEntity)
+        return self
+    }
+    
+}
