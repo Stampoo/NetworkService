18c22dbc6a6a847218be8f11e49a021092558674
Ilya Knyazkov <fivecoil@gmail.com>
2022-03-08 00:00:46 +0300

    Create RequestBuilder

---
+++ Sources/NetworkService/NewNetworkLayer/RequestBuilder/protocols/RequestBuilderProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  RequestBuilderProtocol.swift
+//  
+//
+//  Created by Князьков Илья on 07.03.2022.
+//
+
+import struct Foundation.URL
+
+public protocol RequestBuilderProtocol {
+    
+    var request: RequestProtocol { get }
+    
+}
