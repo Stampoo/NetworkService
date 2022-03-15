e3934405e221ed38dd7129e9b1a1623a52ddaf63
Ilya Knyazkov <fivecoil@gmail.com>
2022-03-15 22:57:04 +0300

    Remove useless layer

--- Sources/NetworkService/NetworkDataTask/NetworkDataTask.swift
+++ Sources/NetworkService/NetworkDataTask/NetworkDataTask.swift
@@ -11,22 +11,28 @@ open class NetworkDataTask {
 
     // MARK: - Private properties
     
-    private let session = NetworkSession()
+    private let core = NetworkCore()
+    private var onComplete: ((Response) -> Void)?
     private var responseContext = Context<Response>()
     
     // MARK: - Public methods
     
     public func startTask(url: URL?,
-                   method: RequestMethod,
-                   parameters: ParametersEncodingType = .query(parameters: [:]),
+                          method: RequestMethod,
+                          parameters: ParametersEncodingType = .query(parameters: [:]),
                           headers: [String: String] = [:]) -> Context<Response> {
         DrainObjectTester.saveWeakReference(on: self)
-        session.start(url: url, method: method, parameters: parameters, headers: headers)
-        session.onComplete { [responseContext] response in
-            responseContext.send(response)
-            DrainObjectTester.saveWeakReference(on: responseContext)
+        do {
+            let requestBuilder = try RequestBuilder(url: url, method: method, parameters: parameters, headers: headers)
+            try core.loadRequest(from: requestBuilder.request) { [responseContext] response in
+                responseContext.send(response)
+                DrainObjectTester.saveWeakReference(on: responseContext)
+            }
+        }
+        catch {
+            responseContext.send(error)
         }
         return responseContext
     }
-    
+
 }
